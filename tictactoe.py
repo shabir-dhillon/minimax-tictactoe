8c358e36b444673f93e5dd611d2ed793e372bdb0
shabir-dhillon <59631293+shabir-dhillon@users.noreply.github.com>
2021-07-28 22:56:16 +0800

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,32 +1,41 @@
 """
 x Create Board
 x Logic to switch empty pieces with symbol
-- compute moves to fill squares
+x compute moves to fill squares
 x compute moves to check for winner
 """
 import random
+from math import inf as infinity 
 
 class T3Board:
     def __init__(self):
-        self.board = [" "," "," "," "," "," "," "," "," "," "]
+        self.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
         self.fillCount = 0
 
     def checkForWin(self):
-        if (self.board[0] == self.board[1] and self.board[0] == self.board[2]) and self.board[0] != " ":
+        if (self.board[0] == self.board[1]
+                and self.board[0] == self.board[2]) and self.board[0] != " ":
             return True
-        if self.board[3] == self.board[4] and self.board[3] == self.board[5] and self.board[3] != " ":
+        if self.board[3] == self.board[4] and self.board[3] == self.board[
+                5] and self.board[3] != " ":
             return True
-        if self.board[6] == self.board[7] and self.board[6] == self.board[8] and self.board[6] != " ":
+        if self.board[6] == self.board[7] and self.board[6] == self.board[
+                8] and self.board[6] != " ":
             return True
-        if self.board[0] == self.board[4] and self.board[0] == self.board[8] and self.board[0] != " ":
+        if self.board[0] == self.board[4] and self.board[0] == self.board[
+                8] and self.board[0] != " ":
             return True
-        if self.board[0] == self.board[3] and self.board[0] == self.board[6] and self.board[0] != " ":
+        if self.board[0] == self.board[3] and self.board[0] == self.board[
+                6] and self.board[0] != " ":
             return True
-        if self.board[1] == self.board[4] and self.board[1] == self.board[7] and self.board[1] != " ":
+        if self.board[1] == self.board[4] and self.board[1] == self.board[
+                7] and self.board[1] != " ":
             return True
-        if self.board[2] == self.board[5] and self.board[2] == self.board[8] and self.board[2] != " ":
+        if self.board[2] == self.board[5] and self.board[2] == self.board[
+                8] and self.board[2] != " ":
             return True
-        if self.board[2] == self.board[4] and self.board[2] == self.board[6] and self.board[2] != " ":
+        if self.board[2] == self.board[4] and self.board[2] == self.board[
+                6] and self.board[2] != " ":
             return True
         return False
 
@@ -99,13 +108,12 @@ class T3Board:
         for i in range(0, 9):
             if self.board[i] == " ":
                 pos.append(i)
-        if len(pos) == 0:
-            return -1
         return pos
 
 
 def getSymbol():
-    player = input("Please select the symbol for Player 1. Enter 1 for X and 2 for O\n")
+    player = input(
+        "Please select the symbol for Player 1. Enter 1 for X and 2 for O\n")
     while True:
         try:
             if int(player) < 1 or int(player) > 2:
@@ -114,13 +122,15 @@ def getSymbol():
                 break
         except ValueError:
             print("Error: Value entered was not a number.\n")
-            player = input("Enter a number in the correct range1 for X or 2 for O: ")
+            player = input(
+                "Enter a number in the correct range1 for X or 2 for O: ")
 
     if int(player) == 1:
         player = "X"
     else:
         player = "O"
     return player
+
 
 def printBoard(board):
     count = 1
@@ -134,145 +144,98 @@ def printBoard(board):
             line += sp + " | "
         count += 1
 
+
 def validateMove(pos):
-  temp = pos
-  while True:
-    try:
-      if int(temp) > 9 or int(temp) < 1:
-        temp = input("Enter a number in between 1-9 that hasn't been played yet: ")
-      else:
-        return int(temp)
-    except ValueError:
-      print("Error: Value entered was not a number.\n")
-      temp = input("Enter a number in the correct range: ")
+    temp = pos
+    while True:
+        try:
+            if int(temp) > 9 or int(temp) < 1:
+                temp = input(
+                    "Enter a number in between 1-9 that hasn't been played yet: "
+                )
+            else:
+                return int(temp)
+        except ValueError:
+            print("Error: Value entered was not a number.\n")
+            temp = input("Enter a number in the correct range: ")
 
 
 def checkGameStatus(player, board):
     if board.checkForWin():
-        print(player + " has won the game!")
-        exit()
+        print("Game over win condition satisified for X or O")
+        new_board = T3Board()
+        mainMenu(new_board)
     elif board.isBoardFilled():
         print("Game is a draw!")
-        exit()
+        new_board = T3Board()
+        mainMenu(new_board)
+
+values = {"X": 10, "O": -10, "tie": 0}
 
 def runCompMove(computer, board):
-    availablePos = board.returnEmptyPositions()
-    bestScore = -100000000
+  depth = len(board.returnEmptyPositions())
+  if depth == 0 or checkGameStatus(computer, board):
+    return
+  if depth == 9:
+    board.increaseFillCount(random.randint(0,8), computer)
+    return
+  else:
+    move = minimax(board, depth, computer)
+    print(str(move))
+    board.increaseFillCount(move, computer)
+
+def minimax(board, depth, player):
+  if player == "X":
     bestMove = -1
-    for move in availablePos:
-        board.increaseFillCount(move, computer)
-        score = minimax(board, False)
-        board.decreaseFillCount(move)
-        if score >= bestScore:
-            bestScore = score
-            bestMove = move
-    board.increaseFillCount(bestMove, computer)
+    bestScore = -infinity
+    next_player = "O"
+  else:
+    bestMove = -1
+    bestScore = infinity
+    next_player = "X"
 
+  current_state = board.checkSimulation("X")
+  
+  if depth == 0 or current_state == "X" or current_state == "O":
+    return -1
+  
+  emptyPos = board.returnEmptyPositions()
+  for pos in emptyPos:
+    board.increaseFillCount(pos, player)
+    score = minimax(board, depth - 1, next_player)
+    board.decreaseFillCount(pos)
 
-def minimax(board, isMaximizing):
-    score = board.checkSimulation("X")
-    if score != None:
-        return values[score]
-    computer = "X"
-    player = "O"
-    emptyPos = board.returnEmptyPositions()
-    if isMaximizing:
-        bestScore = -10000000
-        for move in emptyPos:
-            board.increaseFillCount(move, computer)
-            score = minimax(board, False)
-            board.decreaseFillCount(move)
-            if score > bestScore:
-                bestScore = score
+    if player == "X":
+      if score > bestScore:
+        bestMove = pos
     else:
-        bestScore = 10000000
-        for move in emptyPos:
-            board.increaseFillCount(move, player)
-            score = minimax(board, True)
-            board.decreaseFillCount(move)
-            if score < bestScore:
-                bestScore = score
-    return bestScore
-
-# def runCompMove(computer, board):
-#     availablePos = board.returnEmptyPositions()
-#     bestScore = -100000000
-#     bestMove = -1
-#     if availablePos == -1:
-#         return
-#     for move in availablePos:
-#         board.increaseFillCount(move, computer)
-#         score = minimax(board, True, computer)
-#         board.decreaseFillCount(move)
-#         if score == None:
-#             continue
-#         if score > bestScore:
-#             bestScore = score
-#             bestMove = move
-#     print("Best score is " + str(score))
-#     board.increaseFillCount(bestMove, computer)
-#
-# """
-# Computer W : 1
-# You W : -1
-# Tie : 0
-# """
-#
-# def minimax(board, isMaximizing, symbol):
-#     score = board.checkSimulation(symbol)
-#     if score == 0 or score == 1 or score == -1:
-#         return score
-#     if symbol == "X":
-#         symbol = "O"
-#     else:
-#         symbol = "X"
-#     emptyPos = board.returnEmptyPositions()
-#     if emptyPos != -1:
-#         emptyPos = []
-#         if isMaximizing:
-#             bestScore = -10000000
-#             for move in emptyPos:
-#                 board.increaseFillCount(move, symbol)
-#                 score = minimax(board, False, symbol)
-#                 board.decreaseFillCount(move)
-#                 if score > bestScore:
-#                     bestScore = score
-#             print("Returning score is" + str(score) + " for " + symbol)
-#             return bestScore
-#         else:
-#             bestScore = 10000000
-#             for move in emptyPos:
-#                 board.increaseFillCount(move, symbol)
-#                 score = minimax(board, True, symbol)
-#                 board.decreaseFillCount(move)
-#                 if score < bestScore:
-#                     bestScore = score
-#             print("Returning score is" + str(score) + " for " + symbol)
-#             return bestScore
-
-values = {
-    "X" : 10,
-    "O" : -10,
-    "tie": 0
-}
+      if score < bestScore:
+        bestMove = pos
+  return bestMove
 
 def playVsComputer(board):
     player = "O"
     computer = "X"
-    runCompMove(computer, board)
     printBoard(board)
     while not board.isBoardFilled():
+        depth = len(board.returnEmptyPositions())
+        if depth == 0 or checkGameStatus(computer, board) or checkGameStatus("O", board):
+            return
         move = validateMove(input("Enter a location from 1 - 9: "))
         while True:
             if board.cellEmpty(move - 1):
                 board.increaseFillCount(move - 1, player)
                 break
             else:
-                move = validateMove(input("Enter a location from 1 - 9. Previous location is occupied: "))
+                move = validateMove(
+                    input(
+                        "Enter a location from 1 - 9. Previous location is occupied: "
+                    ))
         checkGameStatus(player, board)
         runCompMove(computer, board)
         printBoard(board)
         checkGameStatus(player, board)
+
 
 def playMultiPlayer(player, board):
     while not board.isBoardFilled():
@@ -282,7 +245,10 @@ def playMultiPlayer(player, board):
                 board.increaseFillCount(move - 1, player)
                 break
             else:
-                move = validateMove(input("Enter a location from 1 - 9. Previous location is occupied: "))
+                move = validateMove(
+                    input(
+                        "Enter a location from 1 - 9. Previous location is occupied: "
+                    ))
         printBoard(board)
         if checkGameStatus(player, board):
             break
@@ -291,21 +257,31 @@ def playMultiPlayer(player, board):
         else:
             player = "X"
 
+
 def mainMenu(board):
-    option = input("Enter M for multiplayer mode,\nC for Computer\nor Q to quit.")
+    option = input(
+        "Enter M for multiplayer mode, C for Computer or Q to quit: ")
     while True:
         if option == "M":
-            print("The pieces on the board have a number from 1 - 9, so level 1 has piece 1, 2, 3 and so on")
+            print(
+                "The pieces on the board have a number from 1 - 9, so level 1 has piece 1, 2, 3 and so on"
+            )
             player = getSymbol()
             playMultiPlayer(player, board)
         elif option == "C":
             print("You will play as O")
-            print("The pieces on the board have a number from 1 - 9, so level 1 has piece 1, 2, 3 and so on")
+            print(
+                "The pieces on the board have a number from 1 - 9, so level 1 has piece 1, 2, 3 and so on"
+            )
+            runCompMove("X", board)
             playVsComputer(board)
         elif option == "Q":
             exit()
         else:
-            option = input("Invalid\nEnter M for multiplayer mode,\nC for Computer\nor Q to quit.")
+            option = input(
+                "Invalid\nEnter M for multiplayer mode,\nC for Computer\nor Q to quit."
+            )
+
 
 print("Welcome to Shabir's TIc Tac Toe!")
 board = T3Board()
